071eb36388f0a944d4e61b24272c2f8a121cdfe3
SayfSbai60 <sayf.sbai.contact@gmail.com>
2025-05-09 16:13:50 +0200

    Update utils.h

--- utils.h
+++ utils.h
@@ -1,6 +1,10 @@
 #ifndef UTILS_H
 #define UTILS_H
-
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <stdbool.h>
 #include "combattant.h"
 
 void afficher_separateur(int longueur);
